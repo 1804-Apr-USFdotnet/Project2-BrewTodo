e1caa1dd6787cd82c0fc06b2c30ce0acea30f812
matthewmagee05 <matthewmagee05@gmail.com>
2018-05-18 11:47:58 -0400

    deleted everything

--- BrewTodo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BrewTodo.MvcApplication" Language="C#" %>
--- BrewTodo/Models/State.cs
+++
@@ -1,8 +0,0 @@
-namespace brewtodo.Models
-{
-    public class State
-    {
-        public int StateID { get; set; }
-        public string StateAbbr { get; set; }
-    }
-}
