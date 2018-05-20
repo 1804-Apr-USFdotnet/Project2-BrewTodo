2f7eec76023f1c036ac92c0c4df997b43a7558e7
matthewmagee05 <matthewmagee05@gmail.com>
2018-05-19 20:05:02 -0400

    Added review repository and controller.  Tested successfully in Postman

---
+++ BrewTodoServer/BrewTodoServer/Controllers/ReviewsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BrewTodoServer;
+using BrewTodoServer.Data;
+using BrewTodoServer.Models;
+
+namespace BrewTodoServer.Controllers
+{
+    public class ReviewsController : ApiController
+    {
+        private ReviewRepository _context = new ReviewRepository(new DbContext());
+
+        // GET: api/Reviews
+        public IQueryable<Review> GetReviews()
+        {
+            return _context.Get();
+        }
+
+        // GET: api/Reviews/5
+        [ResponseType(typeof(Review))]
+        public IHttpActionResult GetReview(int id)
+        {
+            var result = _context.Get(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        // PUT: api/Reviews/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutReview(int id, Review review)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = _context.Put(id, review);
+            if (result == false)
+            {
+                return NotFound();
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Reviews
+        [ResponseType(typeof(Review))]
+        public IHttpActionResult PostReview(Review review)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Post(review);
+
+            return CreatedAtRoute("DefaultApi", new { id = review.ReviewID }, review);
+        }
+
+        // DELETE: api/Reviews/5
+        [ResponseType(typeof(Review))]
+        public IHttpActionResult DeleteReview(int id)
+        {
+
+            var result = _context.Delete(id);
+            if (result == false)
+            {
+                return NotFound();
+            }
+            
+            return Ok();
+        }
+
+       
+    }
+}
---
+++ BrewTodoServer/BrewTodoServer/Data/ReviewRepository.cs
@@ -0,0 +1,95 @@
+﻿using BrewTodoServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace BrewTodoServer.Data
+{
+    public class ReviewRepository : IRepository<Review>, IDisposable
+    {
+        private DbContext db = new DbContext();
+
+        public ReviewRepository(DbContext db)
+        {
+            this.db = db;
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return db.Reviews.Count(e => e.ReviewID == id) > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return false;
+            }
+
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+
+            return true;
+
+        }
+
+        public IQueryable<Review> Get()
+        {
+            return db.Reviews;
+        }
+
+        public Review Get(int id)
+        {
+
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return null;
+            }
+            return review;
+        }
+
+        public void Post(Review review)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+        }
+
+        public bool Put(int id, Review review)
+        {
+            if (db.Reviews.Where(a => a.ReviewID == id).FirstOrDefault() == null)
+            {
+                return false;
+            }
+
+            review.ReviewID = id;
+            Review oldReview = db.Reviews.Where(a => a.ReviewID == id).FirstOrDefault();
+            db.Entry(oldReview).CurrentValues.SetValues(review);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReviewExists(id))
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return true;
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)db).Dispose();
+        }
+    }
+}
