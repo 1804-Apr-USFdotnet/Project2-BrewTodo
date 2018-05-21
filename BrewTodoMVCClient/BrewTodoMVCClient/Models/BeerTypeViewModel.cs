17d946139013a9e0e134eb0fa7661c03fb706b6e
pjrivera08 <pj.rivera08@gmail.com>
2018-05-21 19:26:42 -0400

    Initial commit in new branch

---
+++ BrewTodoMVCClient/BrewTodoMVCClient/Models/BeerTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrewTodoMVCClient.Models
+{
+    public class BeerTypeViewModel
+    {
+        public int BeerTypeID { get; set; }
+        public string BeerTypeName { get; set; }
+    }
+}
---
+++ BrewTodoMVCClient/BrewTodoMVCClient/Models/BreweryViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrewTodoMVCClient.Models
+{
+    public class BreweryViewModel
+    {
+        public int BreweryID { get; set; }
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public string ImageURL {get; set;}
+        public string Address {get; set;}
+        public string ZipCode{get; set;}
+        public int StateID { get; set; }
+        public string PhoneNumber {get; set;}
+        public string BusinessHours { get; set; }
+        public bool HasTShirt {get; set;}
+        public bool HasMug {get; set;}
+        public bool HasGrowler {get; set;}
+        public bool HasFood {get; set;}
+        public double AverageRating { get; set; }
+
+        public State State { get; set; }
+
+        public ICollection<ReviewViewModel> Reviews { get; set; }
+        public ICollection<BeerViewModel> Beers { get; set; }
+    }
+}
