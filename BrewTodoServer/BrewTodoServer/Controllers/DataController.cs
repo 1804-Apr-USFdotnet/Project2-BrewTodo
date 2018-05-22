d879e933b51b1827cef7816a06022fba70c74cd8
Kay Choi <kay.n.choi@gmail.com>
2018-05-22 14:31:01 -0400

    Pushing authentication fix test.

--- BrewTodoServer/BrewTodoServer/Controllers/DataController.cs
+++ BrewTodoServer/BrewTodoServer/Controllers/DataController.cs
@@ -6,34 +6,33 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace BrewTodoServer.Controllers
 {
-    
+    public class DataController : ApiController
+    {
+        private readonly UserRepository _context = new UserRepository();
 
-    
-        public class DataController : ApiController
+        public IHttpActionResult Get()
         {
-            private UserRepository _context = new UserRepository();
+            var user = Request.GetOwinContext().Authentication.User;
 
-            public IHttpActionResult Get()
+            string username = user.Identity.Name;
+
+            bool isAdmin = user.IsInRole("admin");
+            var users = _context.Get().ToList();
+            Models.User result = users.Where(r => r.IdentityID == User.Identity.GetUserId()).FirstOrDefault();
+
+            List<string> roles = user.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value.ToString()).ToList();
+
+            if (result == null)
             {
-                var user = Request.GetOwinContext().Authentication.User;
-
-                string username = user.Identity.Name;
-
-                bool isAdmin = user.IsInRole("admin");
-
-                var id = user.Identity.GetUserId();
-
-                var result = _context.Get().FirstOrDefault(r => r.IdentityID == id);
-
-                List<string> roles = user.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value.ToString()).ToList();
-
-           // return Ok($"Authenticated {username}, {result?.UserID} with roles: [{string.Join(", ", roles)}]!");
-                return Ok(result?.UserID);
+                return StatusCode(HttpStatusCode.Unauthorized);
             }
+
+            return Ok($"Authenticated {username}, The UserID is {result} with roles: [{string.Join(", ", roles)}]!");
         }
     }
+}
