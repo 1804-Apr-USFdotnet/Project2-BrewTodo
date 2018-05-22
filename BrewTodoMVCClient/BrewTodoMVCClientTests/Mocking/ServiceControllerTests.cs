98b32e61295d4d8f336cdb6b264ba7e44be793ab
Kay Choi <kay.n.choi@gmail.com>
2018-05-22 11:58:45 -0400

    Added stub tests for controllers.

---
+++ BrewTodoMVCClient/BrewTodoMVCClientTests/Mocking/ServiceControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using BrewTodoMVCClient;
+using BrewTodoMVCClient.Controllers;
+using BrewTodoMVCClient.Models;
+using System;
+using NUnit.Framework;
+
+namespace BrewTodoServerTests.Mocking
+{
+    [TestFixture]
+    public class ServiceControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void CreateRequestToService()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
