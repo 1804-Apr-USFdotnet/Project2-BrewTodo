1ed31c86eefb3eb719686348971d495c07149588
Kay Choi <kay.n.choi@gmail.com>
2018-05-22 18:21:20 -0400

    Index page passes login state to ViewBag.

--- BrewTodoMVCClient/BrewTodoMVCClient/Controllers/HomeController.cs
+++ BrewTodoMVCClient/BrewTodoMVCClient/Controllers/HomeController.cs
@@ -8,52 +8,52 @@ namespace BrewTodoMVCClient.Controllers
 {
     public class HomeController : ServiceController
     {
-        
-            public async Task<ActionResult> Index()
-            {
-                HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Get, "api/Data");
+        public async Task<ActionResult> Index()
+        {
+            HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Get, "api/Data");
 
-                HttpResponseMessage apiResponse;
-                try
-                {
-                    apiResponse = await HttpClient.SendAsync(apiRequest);
-                }
-                catch
+            HttpResponseMessage apiResponse;
+            try
+            {
+                apiResponse = await HttpClient.SendAsync(apiRequest);
+            }
+            catch
+            {
+                return View("Error");
+            }
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                if (apiResponse.StatusCode != HttpStatusCode.Unauthorized)
                 {
                     return View("Error");
                 }
-                
-                
-                if (!apiResponse.IsSuccessStatusCode)
-                {
-                    if (apiResponse.StatusCode != HttpStatusCode.Unauthorized)
-                    {
-                        return View("Error");
-                    }
-                    ViewBag.Message = "Not logged in!";
-                }
-                else
-                {
-                    var contentString = await apiResponse.Content.ReadAsStringAsync();
-                   
-                    ViewBag.Message = "Logged in! Result: " + contentString;
-                }
+                ViewBag.Message = "Not logged in!";
+                ViewBag.LogIn = false;
+            }
+            else
+            {
+                var contentString = await apiResponse.Content.ReadAsStringAsync();
+
+                ViewBag.Message = "Logged in! Result: " + contentString;
+                ViewBag.LogIn = true;
+            }
 
             return View();
         }
 
-            public ActionResult About()
-            {
-                ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-                return View();
-            }
+            return View();
+        }
 
-            public ActionResult Contact()
-            {
-                ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-                return View();
-            }
+            return View();
         }
     }
+}
