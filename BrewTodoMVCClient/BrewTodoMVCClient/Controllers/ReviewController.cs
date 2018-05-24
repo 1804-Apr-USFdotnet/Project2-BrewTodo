5111efa2ab7816b77d4fe331047575db7de01e74
pjrivera08 <pj.rivera08@gmail.com>
2018-05-24 16:50:50 -0400

    Initial Commint ReviewController in its infancy

---
+++ BrewTodoMVCClient/BrewTodoMVCClient/Controllers/ReviewController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrewTodoMVCClient.Controllers
+{
+    public class ReviewController : Controller
+    {
+        
+
+        // GET: Review/Create
+        public ActionResult Create(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        // POST: Review/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
