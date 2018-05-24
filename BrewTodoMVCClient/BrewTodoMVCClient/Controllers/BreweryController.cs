a00896bf78602f43ec229cfb3d70a241ccfd547b
pjrivera08 <pj.rivera08@gmail.com>
2018-05-23 21:04:31 -0400

    Created new branch to work on PostBrewery Feature

--- BrewTodoMVCClient/BrewTodoMVCClient/Controllers/BreweryController.cs
+++ BrewTodoMVCClient/BrewTodoMVCClient/Controllers/BreweryController.cs
@@ -42,10 +42,65 @@ namespace BrewTodoMVCClient.Controllers
             return View(breweries);
         }
 
-        //POST: Brewery
         public ActionResult CreateBrewery()
         {
-            return View("Not implemented");
+            return View();
+        }
+
+        //POST: Brewery
+        [HttpPost]
+        public ActionResult CreateBrewery(FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var state = new State
+                    {
+                        StateID = 1,
+                        StateAbbr = "Fl"
+                    };
+                    BreweryViewModel brewery = new BreweryViewModel
+                    {
+                        Name = collection["Name"],
+                        Description = collection["Description"],
+                        ImageURL = collection["ImageURL"],
+                        ZipCode = collection["ZipCode"],
+                        StateID = 10, //They should only be able to pick florida right now. Maybe still show the text field but make it uneditable
+                        PhoneNumber = collection["PhoneNumber"],
+                        BusinessHours = collection["BusinessHours"],
+                        HasTShirt = Convert.ToBoolean(collection["HasTShirt"].Split(',')[0]),
+                        HasMug = Convert.ToBoolean(collection["HasMug"].Split(',')[0]),
+                        HasGrowler = Convert.ToBoolean(collection["HasGrowler"].Split(',')[0]),
+                        HasFood = Convert.ToBoolean(collection["HasFood"].Split(',')[0]),
+                        AverageRating = 0,
+                        State = state
+                        
+                    };
+
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + "/api/breweries");
+                        var postTask = client.PostAsJsonAsync<BreweryViewModel>("breweries",brewery);
+                        postTask.Wait();
+
+                        var result = postTask.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Breweries");
+                        }
+                        return View("Non-success Status Code returned");
+                    }
+                }
+                catch (Exception e)
+                {
+                    return View("Caught Exception");
+                }
+            }
+            else
+            {
+                return View("Invalid Model State");
+            }
         }
         
     }
