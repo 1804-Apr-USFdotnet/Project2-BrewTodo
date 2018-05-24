5b2e36c1d4b8bfc2f186a7cdc7d0a16cc4d56b53
pjrivera08 <pj.rivera08@gmail.com>
2018-05-23 23:24:13 -0400

    EditBrewery returns 405 method not allowed

--- BrewTodoMVCClient/BrewTodoMVCClient/Controllers/BreweryController.cs
+++ BrewTodoMVCClient/BrewTodoMVCClient/Controllers/BreweryController.cs
@@ -19,7 +19,7 @@ namespace BrewTodoMVCClient.Controllers
             {
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString()+"/api/breweries");
                 //client.BaseAddress = new Uri("http://localhost:56198/api/breweries/");   
-                //HTTP GET
+               
                 var responseTask = client.GetAsync("breweries");
                 responseTask.Wait();
                 
@@ -57,7 +57,7 @@ namespace BrewTodoMVCClient.Controllers
                 {
                     var state = new State
                     {
-                        StateID = 1,
+                        StateID = 10,
                         StateAbbr = "Fl"
                     };
                     BreweryViewModel brewery = new BreweryViewModel
@@ -76,7 +76,6 @@ namespace BrewTodoMVCClient.Controllers
                         HasFood = Convert.ToBoolean(collection["HasFood"].Split(',')[0]),
                         AverageRating = 0,
                         State = state
-                        
                     };
 
                     using (var client = new HttpClient())
@@ -103,6 +102,85 @@ namespace BrewTodoMVCClient.Controllers
             {
                 return View("Invalid Model State");
             }
+        }
+
+
+        //PUT: Brewery
+        [HttpPost]
+        public ActionResult EditBrewery(int id,FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var state = new State
+                    {
+                        StateID = 10,
+                        StateAbbr = "Fl"
+                    };
+                    BreweryViewModel brewery = new BreweryViewModel
+                    {
+                        BreweryID = id,
+                        Name = collection["Name"],
+                        Description = collection["Description"],
+                        ImageURL = collection["ImageURL"],
+                        Address = collection["Address"],
+                        ZipCode = collection["ZipCode"],
+                        StateID = 10, //They should only be able to pick florida right now. Maybe still show the text field but make it uneditable
+                        PhoneNumber = collection["PhoneNumber"],
+                        BusinessHours = collection["BusinessHours"],
+                        HasTShirt = Convert.ToBoolean(collection["HasTShirt"].Split(',')[0]),
+                        HasMug = Convert.ToBoolean(collection["HasMug"].Split(',')[0]),
+                        HasGrowler = Convert.ToBoolean(collection["HasGrowler"].Split(',')[0]),
+                        HasFood = Convert.ToBoolean(collection["HasFood"].Split(',')[0]),
+                        State = state
+                    };
+
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + "/api/breweries");
+                        //client.BaseAddress = new Uri("http://localhost:56198/api/breweries/");
+                        var putTask = client.PutAsJsonAsync<BreweryViewModel>($"breweries/{id}", brewery);
+                        putTask.Wait();
+
+                        var result = putTask.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Breweries");
+                        }
+                        return View("Non-success Status Code returned");
+                    }
+                }
+                catch (Exception e)
+                {
+                    return View("Caught Exception");
+                }
+            }
+            else
+            {
+                return View("Invalid Model State");
+            }
+        }
+
+        public ActionResult EditBrewery(int id)
+        {
+            BreweryViewModel brewery = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + "/api/breweries");
+                //client.BaseAddress = new Uri("http://localhost:56198/api/breweries/");
+                var responseTask = client.GetAsync("breweries");
+                responseTask.Wait();
+                
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<BreweryViewModel>>();
+                    readTask.Wait();
+                    brewery = readTask.Result.Where(x=>x.BreweryID == id).FirstOrDefault();
+                }
+            }
+            return View(brewery);
         }
         
     }
