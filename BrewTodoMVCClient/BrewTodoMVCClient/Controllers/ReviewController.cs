323d520e404e90fade5fd5ef7d201d421d20905d
pjrivera08 <pj.rivera08@gmail.com>
2018-05-24 23:26:12 -0400

    Create Review works with hardcoded UserId

--- BrewTodoMVCClient/BrewTodoMVCClient/Controllers/ReviewController.cs
+++ BrewTodoMVCClient/BrewTodoMVCClient/Controllers/ReviewController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BrewTodoMVCClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,28 +10,96 @@ namespace BrewTodoMVCClient.Controllers
 {
     public class ReviewController : Controller
     {
-        
-
         // GET: Review/Create
-        public ActionResult Create(int id)
+        // in this case id parameter is the brewery id passed in the url
+        //This is because in routConfig our only route is looking for a parameter specifically named 'id'
+        public ActionResult Create(int id,int userId = 1) 
         {
-            ViewBag.id = id;
             return View();
         }
 
         // POST: Review/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(FormCollection collection,int id,int? userId)
         {
-            try
+            UserViewModel user = null;
+            BreweryViewModel brewery;
+            if(userId == null)
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                userId = 1;
             }
-            catch
+
+            using (var client = new HttpClient())
             {
-                return View();
+                client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + "/api/");
+                var responseTask = client.GetAsync($"users/{userId}");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<UserViewModel>();
+                    readTask.Wait();
+                    user = readTask.Result;
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Server error, no user found.");
+                }
+                
+
+                responseTask = client.GetAsync("breweries/" + id);
+                responseTask.Wait();
+                result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<BreweryViewModel>();
+                    readTask.Wait();
+
+                    brewery = readTask.Result;
+                }
+                else
+                {
+                    brewery = new BreweryViewModel();
+
+                    ModelState.AddModelError(string.Empty, "Server error, no brewery found.");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ReviewViewModel review = new ReviewViewModel
+                    {
+                        BreweryID = id,
+                        Rating = Convert.ToInt32(collection["Rating"]),
+                        ReviewDescription = collection["ReviewDescription"],
+                        User = user,
+                        UserID = (int)userId,
+                        Brewery = brewery,
+                    };
+                    using(var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + "api/");
+                        var postTask = client.PostAsJsonAsync<ReviewViewModel>("reviews", review);
+                        postTask.Wait();
+
+                        var result = postTask.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            //return RedirectToRoute("Details",id);
+                            return RedirectToAction("Details", "Brewery", new { id = id });
+                        }
+                        return View("Non-success Statuse Code returned");
+                    }
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View("Invalid Model State");
             }
         }
 
