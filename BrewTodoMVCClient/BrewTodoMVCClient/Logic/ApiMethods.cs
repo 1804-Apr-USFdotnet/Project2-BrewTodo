83b4f92eb0e919a9cb92d8f2a8ab6412fcd1519c
pjrivera08 <pj.rivera08@gmail.com>
2018-05-26 14:01:11 -0400

    potentially managed to pass hardcoded cookie into header

--- BrewTodoMVCClient/BrewTodoMVCClient/Logic/ApiMethods.cs
+++ BrewTodoMVCClient/BrewTodoMVCClient/Logic/ApiMethods.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace BrewTodoMVCClient.Logic
 {
     public class ApiMethods : IApiMethods
     {
+        string cookieValue = "jIcdxxP-spgk4XGrFy_oV96EqwkF5OgRavyT5t84Zl7_xCkL3zWNMU67qc4actcBm4KJFcMa6hVObWxL3oRz2ct2owVLTCjAkeoPP6LKPo8s6k0wxAEiFWh-PVXZwfM7Lvofli5KIe7f4svMETDi8rZbeybDTu-voeR4q8809OrmgyjtBbao210KWbk2eWq0SxZ90lXPPQumnVm0AHRBaV6x_L788-2Rzjyu3xD2qxyyqtuRYnkclICupeosPo1qb2Xm09tmrd0i6qUOWNBoN2bpERZKzMrNrBTNBij3KtGOfIhtjq0D8QgMOlF6Coyaya7C7Vs8WyWZUUt2q30sq3GoGpprQYtmAh3q4VQfoOOEsXqE0fyBvDtL6vUawGRsarmcQUMhSzTj9jgVwZgpAQSjYHeVApYIeiAuUCLm89NJ-yzvO2ax7eufE2kxy-GefsYkx8Z8teuNkIZxWeJQxbH780ZrMjMLOZhvZ271T4FgA1t0wX1aORuACZdG5i08";
         public ICollection<T> HttpGetFromApi<T>(string apiString)
         {
             ICollection<T> resultList = null;
@@ -32,10 +34,11 @@ namespace BrewTodoMVCClient.Logic
         }
         public void HttpPostToApi<T>(T model, string apiString)
         {
-            using (var client = new HttpClient())
+            using (var client = new HttpClient(new HttpClientHandler { UseCookies = false }))
             {
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + $"/api/{apiString}");
-                var postTask = client.PostAsJsonAsync<T>($"{apiString}", model);
+                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
+                var postTask = client.PostAsJsonAsync<T>($"{apiString}", model); //google how do i add cookie header to PostAsJsonAsync
                 postTask.Wait();
 
                 var result = postTask.Result;
@@ -51,9 +54,10 @@ namespace BrewTodoMVCClient.Logic
         }
         public void HttpPostToApi<T>(T model, string apiController,string apiAction)
         {
-            using (var client = new HttpClient())
+            using (var client = new HttpClient(new HttpClientHandler { UseCookies = false }))
             {
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + $"/api/{apiController}");
+                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
                 var postTask = client.PostAsJsonAsync<T>($"{apiAction}", model);
                 postTask.Wait();
 
