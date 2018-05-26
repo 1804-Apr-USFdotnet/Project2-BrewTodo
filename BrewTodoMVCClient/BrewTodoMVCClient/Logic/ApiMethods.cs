23b3a573a1e18038ccdf8a4e6cdef4810df2e8d1
pjrivera08 <pj.rivera08@gmail.com>
2018-05-26 16:47:45 -0400

    Implemented IsCookieNull() method for cookie check

--- BrewTodoMVCClient/BrewTodoMVCClient/Logic/ApiMethods.cs
+++ BrewTodoMVCClient/BrewTodoMVCClient/Logic/ApiMethods.cs
@@ -4,12 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web;
 
 namespace BrewTodoMVCClient.Logic
 {
     public class ApiMethods : IApiMethods
     {
-        string cookieValue = "jIcdxxP-spgk4XGrFy_oV96EqwkF5OgRavyT5t84Zl7_xCkL3zWNMU67qc4actcBm4KJFcMa6hVObWxL3oRz2ct2owVLTCjAkeoPP6LKPo8s6k0wxAEiFWh-PVXZwfM7Lvofli5KIe7f4svMETDi8rZbeybDTu-voeR4q8809OrmgyjtBbao210KWbk2eWq0SxZ90lXPPQumnVm0AHRBaV6x_L788-2Rzjyu3xD2qxyyqtuRYnkclICupeosPo1qb2Xm09tmrd0i6qUOWNBoN2bpERZKzMrNrBTNBij3KtGOfIhtjq0D8QgMOlF6Coyaya7C7Vs8WyWZUUt2q30sq3GoGpprQYtmAh3q4VQfoOOEsXqE0fyBvDtL6vUawGRsarmcQUMhSzTj9jgVwZgpAQSjYHeVApYIeiAuUCLm89NJ-yzvO2ax7eufE2kxy-GefsYkx8Z8teuNkIZxWeJQxbH780ZrMjMLOZhvZ271T4FgA1t0wX1aORuACZdG5i08";
+        public bool IsCookieNull()
+        {
+            string cookieValue;
+            if (HttpContext.Current.Request.Cookies["AuthTestCookie"] != null)
+            {
+                cookieValue = HttpContext.Current.Request.Cookies["AuthTestCookie"].Value;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         public ICollection<T> HttpGetFromApi<T>(string apiString)
         {
             ICollection<T> resultList = null;
@@ -36,8 +49,14 @@ namespace BrewTodoMVCClient.Logic
         {
             using (var client = new HttpClient(new HttpClientHandler { UseCookies = false }))
             {
+                string cookieValue;
+                if (HttpContext.Current.Request.Cookies["AuthTestCookie"] != null)
+                {
+                    cookieValue = HttpContext.Current.Request.Cookies["AuthTestCookie"].Value;
+                    client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
+
+                }
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + $"/api/{apiString}");
-                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
                 var postTask = client.PostAsJsonAsync<T>($"{apiString}", model); //google how do i add cookie header to PostAsJsonAsync
                 postTask.Wait();
 
@@ -56,8 +75,13 @@ namespace BrewTodoMVCClient.Logic
         {
             using (var client = new HttpClient(new HttpClientHandler { UseCookies = false }))
             {
+                string cookieValue;
+                if (HttpContext.Current.Request.Cookies["AuthTestCookie"] != null)
+                {
+                    cookieValue = HttpContext.Current.Request.Cookies["AuthTestCookie"].Value;
+                    client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
+                }
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + $"/api/{apiController}");
-                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
                 var postTask = client.PostAsJsonAsync<T>($"{apiAction}", model);
                 postTask.Wait();
 
@@ -76,8 +100,13 @@ namespace BrewTodoMVCClient.Logic
         {
             using (var client = new HttpClient(new HttpClientHandler { UseCookies = false }))
             {
+                string cookieValue;
+                if (HttpContext.Current.Request.Cookies["AuthTestCookie"] != null)
+                {
+                    cookieValue = HttpContext.Current.Request.Cookies["AuthTestCookie"].Value;
+                    client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
+                }
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + $"api/{apiString}/");
-                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
                 var postTask = client.PutAsJsonAsync<T>($"{id}", model);
                 postTask.Wait();
 
@@ -93,11 +122,17 @@ namespace BrewTodoMVCClient.Logic
             }
         }
         public void HttpDeleteFromApi<T>(T model, string apiString, int id)
-        {
+        {  
             using (var client = new HttpClient(new HttpClientHandler { UseCookies = false }))
             {
+                string cookieValue;
+                if (HttpContext.Current.Request.Cookies["AuthTestCookie"] != null)
+                {
+                    cookieValue = HttpContext.Current.Request.Cookies["AuthTestCookie"].Value;
+                    client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
+
+                }
                 client.BaseAddress = new Uri(ServiceController.serviceUri.ToString() + $"api/{apiString}/");
-                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("AuthTestCookie", cookieValue).ToString());
                 var deleteTask = client.DeleteAsync($"{id}");
                 deleteTask.Wait();
                 var result = deleteTask.Result;
